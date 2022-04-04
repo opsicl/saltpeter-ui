a8259ecab329d344150c7fb7d952ab02ba9788f4
opsicl <opsicl@pop-os.localdomain>
2022-04-04 14:57:15 +0300

    test

--- src/components/cronJobsDisplay/socket.js
+++ src/components/cronJobsDisplay/socket.js
@@ -1,5 +1,6 @@
 import ReconnectingWebSocket from 'reconnecting-websocket';
 
 const SALTPETER_WS = "ws://" + new URL(window.location.href).host  + "/ws";
+
 export const socket = new ReconnectingWebSocket(SALTPETER_WS);
 
