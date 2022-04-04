fa57dd0095d8982c98a841ffb1a6e44c222c17f3
opsicl <opsicl@pop-os.localdomain>
2022-04-04 14:43:34 +0300

    websocket update

--- src/components/cronJobsDisplay/socket.js
+++ src/components/cronJobsDisplay/socket.js
@@ -1,6 +1,5 @@
 import ReconnectingWebSocket from 'reconnecting-websocket';
 
-let apis = require("../../apis.json");
-const SALTPETER_WS = apis.saltpeter_ws;
+const SALTPETER_WS = "ws://" + new URL(window.location.href).host  + "/ws";
 export const socket = new ReconnectingWebSocket(SALTPETER_WS);
 
