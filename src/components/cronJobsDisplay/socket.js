626890acfd303e0315ebe6132e6dd600f4db2868
root <root@template>
2023-02-13 12:06:26 +0200

    fixed socket

--- src/components/cronJobsDisplay/socket.js
+++ src/components/cronJobsDisplay/socket.js
@@ -8,7 +8,5 @@ if (window.location.href.indexOf("https://") > -1) {
     SALTPETER_WS = "ws://" + new URL(window.location.href).host  + "/ws";
 }
 
-SALTPETER_WS = "ws://salt.opsi.cl:8889/ws"
-
 export const socket = new ReconnectingWebSocket(SALTPETER_WS);
 
