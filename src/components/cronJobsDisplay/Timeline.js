3887f027bed331f9a2655acd0aaaae716a47c7ca
root <root@template>
2024-01-11 17:53:02 +0200

    react chart

--- src/components/cronJobsDisplay/Timeline.js
+++ src/components/cronJobsDisplay/Timeline.js
@@ -3,6 +3,9 @@ import CronJobTimeline from "./CronJobTimeline";
 import "./Timeline.css";
 import { socket } from "./socket.js";
 import { Link } from "react-router-dom";
+import ReactApexChart from 'react-apexcharts';
+
+import moment from 'moment';
 
 let apis = require("../../version.json");
 const UI_VERSION = apis.version;
@@ -45,6 +48,93 @@ class Timeline extends React.Component {
       column_desc_sort:"",
       active_columns:[],
       config_received: false,
+      series: [
+              {
+                data: [
+                  {
+                    x: 'Analysis',
+                    y: [
+                      new Date('2019-02-27').getTime(),
+                      new Date('2019-03-04').getTime()
+                    ],
+                    fillColor: '#008FFB'
+                  },
+                  {
+                    x: 'Design',
+                    y: [
+                      new Date('2019-03-04').getTime(),
+                      new Date('2019-03-08').getTime()
+                    ],
+                    fillColor: '#00E396'
+                  },
+                  {
+                    x: 'Coding',
+                    y: [
+                      new Date('2019-03-07').getTime(),
+                      new Date('2019-03-10').getTime()
+                    ],
+                    fillColor: '#775DD0'
+                  },
+                  {
+                    x: 'Testing',
+                    y: [
+                      new Date('2019-03-08').getTime(),
+                      new Date('2019-03-12').getTime()
+                    ],
+                    fillColor: '#FEB019'
+                  },
+                  {
+                    x: 'Deployment',
+                    y: [
+                      new Date('2019-03-12').getTime(),
+                      new Date('2019-03-17').getTime()
+                    ],
+                    fillColor: '#FF4560'
+                  }
+                ]
+              }
+            ],
+            options: {
+              chart: {
+                height: 350,
+                type: 'rangeBar'
+              },
+              plotOptions: {
+                bar: {
+                  horizontal: true,
+                  distributed: true,
+                  dataLabels: {
+                    hideOverflowingLabels: false
+                  }
+                }
+              },
+              dataLabels: {
+                enabled: true,
+                formatter: function(val, opts) {
+                  var label = opts.w.globals.labels[opts.dataPointIndex]
+                  var a = moment(val[0])
+                  var b = moment(val[1])
+                  var diff = b.diff(a, 'days')
+                  return label + ': ' + diff + (diff > 1 ? ' days' : ' day')
+                },
+                style: {
+                  colors: ['#f3f4f5', '#fff']
+                }
+              },
+              xaxis: {
+                type: 'datetime'
+              },
+              yaxis: {
+                show: false
+              },
+              grid: {
+                row: {
+                  colors: ['#f3f4f5', '#fff'],
+                  opacity: 1
+                }
+              }
+            },
+
     };
     socket.debug=true;
     socket.timeoutInterval = 5400;
@@ -496,6 +586,9 @@ class Timeline extends React.Component {
         <div className = "versions">
             <p>UI: {UI_VERSION}</p>
             <p>Backend: {this.state.backend_version}</p>
+        </div>
+        <div id="chart">
+            <ReactApexChart options={this.state.options} series={this.state.series} type="rangeBar" height={350} />
         </div>
       </div>
     );
